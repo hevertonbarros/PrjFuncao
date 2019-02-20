3c284bebb30d80d6924df159e5fcb8854d5fe6f4
Heverton Barros <hevertonbarros@gmail.com>
2019-02-20 17:36:07 -0300

    Commit inicial

--- index.php
+++ index.php
@@ -6,46 +6,7 @@ and open the template in the editor.
 -->
 
 <?php
-
-class car{
-    private $marca;
-    private $valor;
-    
-    public function __construct() {
-        $this->marca = "Fiat";
-        $this->valor = 123000;
-    }
-    
-    function mostrarCarro() {
-        echo 'Marca:' . $this->marca . '<br>';
-        echo 'Valor:' . $this->valor . '<br>';
-    }
-    
-    function setMarca($marca) {
-        $this->marca = $marca;
-    }
-    
-    function getMarca() {
-        return $this->marca;
-    }
-  
-    function getValor() {
-        return $this->valor;
-    }
-
-    function setValor($valor) {
-        $this->valor = $valor;
-    }    
-}
-
-function soma($a, $b) {
-    return $a + $b;
-}
-
-function referencia(&$a) {
-    $a=10;
-}
-
+include_once "funcoesDeTeste.php";
 ?>
 
 <html>
